cda68152f847f8c3725bcd2a259f2ac32f855707
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-02-12 13:53:07 +0530

    Tailwind Installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="text-4xl text-orange-400">Hello World</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
