afd1898ded17aa91cde5926cb7c719bda6e5969a
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-02-15 11:39:52 +0530

    .env file added and hidden using git-ignore

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="text-6xl">Welcome!</div>;
+  return (
+    <div className="text-6xl">
+      Welcome!
+      <p>{process.env.REACT_APP_GITHUB_TOKEN}</p> {/*checking env token*/}
+    </div>
+  );
 };
 
 export default Home;
