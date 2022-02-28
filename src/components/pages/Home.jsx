1859344d837a6c112550fff66df4236c74afe7a3
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-02-28 20:09:12 +0530

    User Search Component Done!

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import UserResults from "../users/UserResults";
+import UserSearch from "../users/UserSearch";
 
 const Home = () => {
   return (
     <>
-      {/* {Searchbar} */}
+      <UserSearch />
       <UserResults />
     </>
   );
