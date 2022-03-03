60621b55a2dda70267b3346d8398f8841ccbfe2d
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-03-03 11:13:55 +0530

    User Profile Stats

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -1,4 +1,4 @@
-import { FaCodepen, FaUserFriends, FaUsers, FaStore } from "react-icons/fa";
+import { FaUserFriends, FaUsers, FaStore, FaCodepen } from "react-icons/fa";
 import { useEffect, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import GithubContext from "../../context/github/GithubContext";
@@ -34,7 +34,7 @@ const User = () => {
     return <Spinner />;
   }
   return (
-    <div>
+    <>
       <div className="w-full mx-auto lg:w-10/12">
         <div className="mb-4">
           <Link to="/" className="btn btn-ghost">
@@ -71,14 +71,87 @@ const User = () => {
                   rel="noreferrer"
                   className="btn btn-outline"
                 >
-                  Visit Profile
+                  Visit Github Profile
                 </a>
               </div>
+            </div>
+            <div className="w-full rounded-lg shadow-md bg-base-100 stats no-scrollbar overflow-x-auto">
+              {location && (
+                <div className="stat">
+                  <div className="stat-title text-md">Location</div>
+                  <div className="stat-value text-lg">{location}</div>
+                </div>
+              )}
+              {blog && (
+                <div className="stat">
+                  <div className="stat-title text-md">Website</div>
+                  <div className="stat-value text-lg">
+                    <a
+                      href={`https://${blog}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {blog}
+                    </a>
+                  </div>
+                </div>
+              )}
+              {twitter_username && (
+                <div className="stat">
+                  <div className="stat-title text-md">Twitter</div>
+                  <div className="stat-value text-lg">
+                    <a
+                      href={`https://twitter.com/${twitter_username}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {twitter_username}
+                    </a>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        <div class="stats w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 no-scrollbar overflow-x-auto">
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <FaUsers className="text-3xl md:text-5xl" />
+            </div>
+            <div class="stat-title pr-5">Followers</div>
+            <div class="stat-value pr-5 text-3xl md:text-4xl">{followers}</div>
+          </div>
+
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <FaUserFriends className="text-3xl md:text-5xl" />
+            </div>
+            <div class="stat-title pr-5">Following</div>
+            <div class="stat-value pr-5 text-3xl md:text-4xl">{following}</div>
+          </div>
+
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <FaCodepen className="text-3xl md:text-5xl" />
+            </div>
+            <div className="stat-title pr-5">Public Repos</div>
+            <div className="stat-value pr-5 text-3xl md:text-4xl">
+              {public_repos}
+            </div>
+          </div>
+
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <FaStore className="text-3xl md:text-5xl" />
+            </div>
+            <div class="stat-title pr-5">Public Gists</div>
+            <div class="stat-value pr-5 text-3xl md:text-4xl">
+              {public_gists}
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
